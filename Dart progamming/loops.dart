5b0b638d4518e4389fc75fa2511dd5a3e1abef21
Folafunmi <folafunmimustapha@gmail.com>
2020-02-19 21:04:15 +0100

    for, for in, forEach loop

---
+++ Dart progamming/loops.dart
@@ -0,0 +1,20 @@
+//standard for loop
+
+void main() {
+  for (var i = 0; i <= 10; ++i) {
+    print(i);
+  }
+
+  var items = [1, 2, 3, 4, 5];
+  //for in loop
+  for (var item in items) {
+    print(item);
+  }
+
+  //forEach loop
+  var numbers = [1, 4, 5, 6];
+  numbers.forEach( (n) => {
+    print(n)
+  });
+}
+
