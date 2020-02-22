0658dc0d73ce085a65df1a73e789470da98c4f25
Folafunmi <folafunmimustapha@gmail.com>
2020-02-22 17:21:14 +0100

    Updated collection.dart

--- Dart progamming/collection.dart
+++ Dart progamming/collection.dart
@@ -24,5 +24,26 @@ void main() {
     print('\n');
   }
 
+  //Set
+  var stuff = {1, 2, 3, 4, 5};
+  var stuff1 = {};
+  var stuff2 = <String>{};
+
+  print(stuff.runtimeType); //HashSet type
+  print(stuff1.runtimeType); //HashMap type
+  print(stuff2.runtimeType); //HashSet type
   
+  //Map
+  var family = {
+    //Key : value
+    'one': 'dad',
+    'two': 'mum',
+    'three': 'bro',
+    'four': 'sis'
+  };
+  print(family['one']);
+
+  //another method of creating maps
+  var family2 = Map();
+  family2['one2'] = 'dad2';
 }
